406f623bae1f09991d202616186ed7ece431d71a
Duplication1 <gamot.kim.fernandez@gmail.com>
2024-11-09 23:01:41 +0800

    header-dropdown

--- views/header.js
+++ views/header.js
@@ -12,45 +12,48 @@ export function header(isLoggedIn) {
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse " id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <li class="nav-item header-nav-item">
             <a class="nav-link" href="./index.php"
               >Home <i class="bi bi-house-fill"></i
             ></a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item header-nav-item">
             <a class="nav-link" href="./services.php"
               >Service <i class="bi bi-person-fill"></i
             ></a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item header-nav-item">
             <a class="nav-link" href="#"
               >Household <i class="bi bi-house-door-fill"></i
             ></a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item header-nav-item">
             <a class="nav-link" href="#"
               >Officials <i class="bi bi-person-badge"></i
             ></a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item header-nav-item">
             <a class="nav-link" href="#"
               >About us <i class="bi bi-person-lines-fill"></i
             ></a>
           </li>
+          
            ${
              typeof isLoggedIn === 'string'
                ? `
-              <div class="dropdown">
-                  <button class="btn nav-item dropdown-toggle" style = "color: yellow;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown button
+
+              <div class="dropdown dropdown-header">
+                  <button class="btn nav-item dropdown-toggle dropdown-btn" style = "color: yellow;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Settings
                   </button>
-                    <div class="dropdown-menu w-25" aria-labelledby="dropdownMenuButton">
+              
+                    <div class="dropdown-menu dropdown-pop-up" aria-labelledby="dropdownMenuButton">
                      <a class="dropdown-item nav-link "  href="#">Account</a>
                      <a class="dropdown-item nav-link" href="SignOut.php">Sign Out</a>
                     </div>
-                </div>
+              </div>
             `
                : `
                <li class='nav-item'>
